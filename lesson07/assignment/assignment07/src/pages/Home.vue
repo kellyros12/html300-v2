08e03afe5441cb5a93fd9861099dda46292c25d7
Kelly Ros <kellyros91@gmail.com>
2022-04-29 12:12:07 -0700

    Tried to make an MPA, but ended up using router

---
+++ lesson07/assignment/assignment07/src/pages/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="jumbotron">
+    <h1 class="display-4">Welcome to the Growler's fanclub!</h1>
+    <p class="lead">GSHL Div 4A Hockey Team</p>
+    <hr class="my-2">
+    <p>Check out photos from our last game!</p>
+    <button type="button" class="btn btn-primary" data-toggle="tooltip" data-placement="bottom" title="This button doesn't actually do anything">
+      Click here!
+    </button>
+  </div>
+</template>
